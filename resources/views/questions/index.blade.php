2de24350998ffd0caa34f01a206b683a0e23411e
Syed Raza <cute.lorian17@gmail.com>
2019-09-15 11:26:57 +0500

    Gate authentication added

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -43,12 +43,17 @@
                                         {{$question->title}}
                                     </h3>
                                     <div class="ml-auto">
+                                        @if(Auth::user()->can('update-question',$question))
                                         <a href="{{ route('questions.edit',$question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endif
+
+                                        @if(Auth::user()->can('delete-question',$question))
                                         <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id)}}">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
                                         </form>
+                                        @endif
                                     </div>
                                 </div>    
                                 <p class="lead">
